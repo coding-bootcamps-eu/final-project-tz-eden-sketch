2e7048e6d01a7577bbcea1e47289a9ca10a63920
nb <github@n-baller.de>
2023-09-30 22:27:49 +0200

    feat: basic styling homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,49 @@ import CardComponent from '@/components/CardComponent.vue'
 </script>
 
 <template>
-  <main>
-    <q-icon name="fa-brands fa-rebel" />
-    <CardComponent />
-  </main>
+  <div class="overview">
+    <!-- <q-icon name="fa-brands fa-rebel" /> -->
+    <header>
+      <h1>Eden Sketch</h1>
+    </header>
+    <main class="card-wrapper">
+      <CardComponent />
+      <CardComponent />
+    </main>
+  </div>
 </template>
+
+<style scoped>
+.overview {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.card-wrapper {
+  display: grid;
+  gap: 1rem;
+
+  grid-template-columns: 1;
+}
+
+@media screen and (min-width: 600px) {
+  /* todo: funktioniert nicht */
+  .card-wrapper {
+    display: grid;
+    gap: 1rem;
+
+    grid-template-columns: 2;
+  }
+}
+
+@media screen and (min-width: 2000px) {
+  /* todo: funktioniert nicht */
+  .card-wrapper {
+    display: grid;
+    gap: 1rem;
+
+    grid-template-columns: 4;
+  }
+}
+</style>
