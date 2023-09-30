ca46415242abf8e0313419a9c452f65c456ec4bd
nb <github@n-baller.de>
2023-09-30 21:56:19 +0200

    feat: card component basic structur and styling

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,62 @@
+<script setup></script>
+
+<template>
+  <div class="card">
+    <div class="content-wrapper">
+      <img class="card-image" src="src/assets/images/bellpepper.webp" />
+
+      <div class="card-infos">
+        <h2 class="card-title">Titel hier</h2>
+        <p class="card-description">Kurze Beschreibung</p>
+      </div>
+    </div>
+    <a href="/plantspecies/view/:plantspecies*" class="card-link"></a>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  overflow: hidden;
+  background: var(--clr-info);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+  position: relative;
+
+  max-width: 50ch;
+}
+
+.card-image {
+  width: 100%;
+  object-fit: contain;
+}
+
+.card-title {
+  font-size: 1rem;
+  font-weight: bold;
+  margin: 0;
+}
+
+.card-description {
+  margin: 0;
+}
+
+.content-wrapper {
+  display: grid;
+  grid-template-rows: auto min-content;
+  padding: 1rem;
+  position: relative;
+}
+
+.card-infos {
+  padding-block: 1rem;
+  margin-inline: auto;
+}
+
+.card-link {
+  /* To stretch the link on top of the card div */
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import CardComponent from '@/components/CardComponent.vue'
+</script>
 
 <template>
   <main>
     <q-icon name="fa-brands fa-rebel" />
+    <CardComponent />
   </main>
 </template>
