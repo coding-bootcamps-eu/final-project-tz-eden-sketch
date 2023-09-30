1263c357e4e809bc8229ed1018bb5b8702374022
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-09-30 10:59:33 +0200

    Feat: Adds user registration component

---
+++ src/components/UserRegistration.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-input class="input__username" outlined v-model="text" label="Username" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const text = ref('')
+</script>
+
+<style scoped>
+html {
+  text-align: center;
+}
+.input__username {
+  width: 30rem;
+  margin: 0 auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import UserRegistration from '../components/UserRegistration.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <UserRegistration />
   </main>
 </template>
