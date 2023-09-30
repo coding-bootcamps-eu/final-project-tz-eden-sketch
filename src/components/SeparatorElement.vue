e25a7279d5e84515e3c1be3106551c05481897e0
nb <github@n-baller.de>
2023-09-30 20:38:04 +0200

    refactoring: used articels and defined separator component

---
+++ src/components/SeparatorElement.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="separator"></div>
+</template>
+
+<style scoped>
+.separator {
+  height: 1px;
+  background-color: var(--clr-secondary);
+  margin-block: 1rem;
+}
+</style>
--- src/views/PlantVarietyView.vue
+++ src/views/PlantVarietyView.vue
@@ -1,9 +1,12 @@
 <script setup>
 // import { ref } from 'vue'
+import SeparatorElement from '@/components/SeparatorElement.vue'
 </script>
+
 <template>
-  <main>
+  <div>
     <q-breadcrumbs color="primary" class="breadcrum">
+      <!-- todo: Verlinkungen anpassen -->
       <template v-slot:separator>
         <q-icon size="1.5em" name="chevron_right" color="primary" />
       </template>
@@ -14,129 +17,129 @@
         to="/plantspecies/view/"
         icon="svguse:/icons.svg#sprout"
       />
-      <!-- todo: verlinkten Path anpassen:Übersicht Pflanzenarten, icon fixen: bei Pflanzenarten soll icon-plants.svg genutzt werden-->
       <q-breadcrumbs-el label="Spinat" />
       <q-breadcrumbs-el label="Spinatsorte" />
     </q-breadcrumbs>
+    <!-- <SeparatorElement /> -->
 
-    <div class="separator"></div>
-    <header class="header">
-      <div class="image-container">
-        <img class="image" src="../assets/images/bellpepper.webp" alt="paprika" />
-      </div>
-      <div class="wrapper-headline">
-        <p class="headline-label">Sorte</p>
-        <h1 class="headline-main headline-plantname">Spinatsorte</h1>
-        <p class="headline-sub botanical-name">Spinacia oleracea</p>
-      </div>
-    </header>
-    <div class="separator"></div>
+    <main>
+      <header class="header">
+        <div class="image-container">
+          <img class="image" src="../assets/images/bellpepper.webp" alt="paprika" />
+        </div>
+        <div class="wrapper-headline">
+          <p class="headline-label">Sorte</p>
+          <h1 class="headline-main headline-plantname">Spinatsorte</h1>
+          <p class="headline-sub botanical-name">Spinacia oleracea</p>
+        </div>
+      </header>
+      <SeparatorElement />
 
-    <h2>Art</h2>
-    <p>Spinat</p>
-    <div class="separator"></div>
+      <article>
+        <h2>Art</h2>
+        <p>Spinat</p>
+      </article>
+      <SeparatorElement />
 
-    <h2>Pflanzenfamilie</h2>
-    <p>Gänsefußgewächse (Chenopodiaceae)</p>
-    <div class="separator"></div>
+      <article>
+        <h2>Pflanzenfamilie</h2>
+        <p>Gänsefußgewächse (Chenopodiaceae)</p>
+      </article>
+      <SeparatorElement />
 
-    <h2>Saisonübersicht</h2>
-    <!---Komponente!!!-->
-    <p>
-      Zeit in Beet / Kulturdauer:
-      <span>Tage</span>
-    </p>
-    <div class="separator"></div>
+      <article>
+        <h2>Saisonübersicht</h2>
+        <ul>
+          <li>Vorziehen: mitte April bis mitte Juni</li>
+          <li>Pflanzung: mitte Mai bis mitte Juli</li>
+          <li>Direktaussat: mitte Mai bis mitte Juni</li>
+          <br />
+          <li>Ernte: Ende Juli bis mitte Oktober</li>
+          <br />
+          <br />
+          <li>Zeit in Beet / Kulturdauer: 120 Tage</li>
+        </ul>
+      </article>
+      <SeparatorElement />
 
-    <h2>Beschreibung</h2>
-    <p>
-      Hier wird die einzelne Spinatsorte beschrieben. Beschreibung Spinat gehört zur Familie der
-      Gänsefußgewächse. Er hat eine sehr kurze Kulturzeit, daher eignet er sich prima als Vor- und
-      Nachkultur. Es gibt ca. 50 Sorten des Frühjahrs- und Winterspinats mit diversen Formen, Farben
-      und unterschiedlicher Fleischigkeit.
-    </p>
-    <div class="separator"></div>
+      <article>
+        <h2>Beschreibung</h2>
+        <p>
+          Hier wird die einzelne Spinatsorte beschrieben. Beschreibung Spinat gehört zur Familie der
+          Gänsefußgewächse. Er hat eine sehr kurze Kulturzeit, daher eignet er sich prima als Vor-
+          und Nachkultur. Es gibt ca. 50 Sorten des Frühjahrs- und Winterspinats mit diversen
+          Formen, Farben und unterschiedlicher Fleischigkeit.
+        </p>
+      </article>
+      <SeparatorElement />
 
-    <h2>Pflege</h2>
-    <ul class="plantcare-details">
-      <li class="info-wrapper">
-        <!-- <img src="" alt="Planzabstand" class="info-icon" /> -->
-        <q-icon name="svguse:/icons.svg#plantdistance" />
-        <p class="info-category">Pflanzabstand</p>
-        <p class="info-value">3 cm</p>
-      </li>
-      <li class="info-wrapper">
-        <img src="" alt="Reihenabstand" class="info-icon" />
-        <p class="info-category">Reihenabstand</p>
-        <p class="info-value">30 cm</p>
-      </li>
-      <li class="info-wrapper">
-        <img src="" alt="Saattiefe" class="info-icon" />
-        <p class="info-category">Saattiefe</p>
-        <p class="info-value">0,5 cm</p>
-      </li>
-      <li class="info-wrapper">
-        <!-- <img src="" alt="Lichtbedarf" class="info-icon" /> -->
-        <q-icon name="bi-sun" />
-        <p class="info-category">Lichtbedarf</p>
-        <p class="info-value">Sonne bis Halbschatten</p>
-      </li>
-      <li class="info-wrapper">
-        <img src="" alt="Nährstoffbedarf" class="info-icon" />
-        <p class="info-category">Nährstoffbedarf</p>
-        <p class="info-value">mittel</p>
-      </li>
-      <li class="info-wrapper">
-        <!-- <img src="" alt="Wasserbedarf" class="info-icon" /> -->
-        <q-icon name="bi-droplet" />
-        <!-- <q-icon name="la-tint" /> geht nicht -->
-        <p class="info-category">Wasserbedarf</p>
-        <p class="info-value">mittel</p>
-      </li>
-    </ul>
-    <div class="separator"></div>
+      <article>
+        <h2>Pflege</h2>
+        <ul class="plantcare-details">
+          <li class="info-wrapper">
+            <!-- <img src="" alt="Planzabstand" class="info-icon" /> -->
+            <q-icon name="svguse:/icons.svg#plantdistance" />
+            <p class="info-category">Pflanzabstand</p>
+            <p class="info-value">3 cm</p>
+          </li>
+          <li class="info-wrapper">
+            <img src="" alt="Reihenabstand" class="info-icon" />
+            <p class="info-category">Reihenabstand</p>
+            <p class="info-value">30 cm</p>
+          </li>
+          <li class="info-wrapper">
+            <img src="" alt="Saattiefe" class="info-icon" />
+            <p class="info-category">Saattiefe</p>
+            <p class="info-value">0,5 cm</p>
+          </li>
+          <li class="info-wrapper">
+            <!-- <img src="" alt="Lichtbedarf" class="info-icon" /> -->
+            <q-icon name="bi-sun" />
+            <p class="info-category">Lichtbedarf</p>
+            <p class="info-value">Sonne bis Halbschatten</p>
+          </li>
+          <li class="info-wrapper">
+            <img src="" alt="Nährstoffbedarf" class="info-icon" />
+            <p class="info-category">Nährstoffbedarf</p>
+            <p class="info-value">mittel</p>
+          </li>
+          <li class="info-wrapper">
+            <!-- <img src="" alt="Wasserbedarf" class="info-icon" /> -->
+            <q-icon name="bi-droplet" />
+            <!-- <q-icon name="la-tint" /> geht nicht -->
+            <p class="info-category">Wasserbedarf</p>
+            <p class="info-value">mittel</p>
+          </li>
+        </ul>
+      </article>
+      <SeparatorElement />
 
-    <h2>Anbautipps</h2>
-    <p>
-      Möhren bevorzugen tiefgründige, lockere Erde mit einem hohen Humusanteil und guter
-      Nährstoffversorgung. Die Aussaat von Früh- und Sommermöhren sollte ab Mitte Februar so früh
-      wie möglich erfolgen, da die Keimung bei 5°C bis zu 45 Tage dauert. Die Keimlinge wachsen sehr
-      langsam und benötigen zu Beginn viel Aufmerksamkeit, damit sie nicht von Unkraut überwachsen
-      werden. Es bietet sich an, neben den Möhrenreihen je eine Reihe Radieschen als Markiersaat zu
-      säen. Nach der Keimung sollten die Pflänzchen ausgedünnt werden.
-    </p>
-    <div class="separator"></div>
+      <article>
+        <h2>Anbautipps</h2>
+        <p>
+          Möhren bevorzugen tiefgründige, lockere Erde mit einem hohen Humusanteil und guter
+          Nährstoffversorgung. Die Aussaat von Früh- und Sommermöhren sollte ab Mitte Februar so
+          früh wie möglich erfolgen, da die Keimung bei 5°C bis zu 45 Tage dauert. Die Keimlinge
+          wachsen sehr langsam und benötigen zu Beginn viel Aufmerksamkeit, damit sie nicht von
+          Unkraut überwachsen werden. Es bietet sich an, neben den Möhrenreihen je eine Reihe
+          Radieschen als Markiersaat zu säen. Nach der Keimung sollten die Pflänzchen ausgedünnt
+          werden.
+        </p>
+      </article>
+      <SeparatorElement />
 
-    <h2>Mischkultur</h2>
-    <h3>
-      <q-icon name="bi-suit-heart-fill"></q-icon>
-      Gute Nachbarn
-    </h3>
-    <ul class="list">
-      <li>Radieschen</li>
-      <li>Erbse</li>
-      <li>Steckrübe</li>
-    </ul>
-
-    <h3>
-      <q-icon name="bi-lightning-fill" color="warning"></q-icon>
-      Schlechte Nachbarn
-    </h3>
-    <ul class="list">
-      <li>Mangold</li>
-      <li>Rote Beete</li>
-      <li>Liebstöckel</li>
-    </ul>
-    <div class="separator"></div>
-
-    <h2>Meine Sätze im Beet</h2>
-    <ul>
-      <li>Satz #1 in Beet 2 <span>(seit mitte März)</span></li>
-      <li>Satz #2 in Beet 2 <span>(seit Ende März)</span></li>
-      <li>Satz #3 in Beet 2 <span>(seit mitte April)</span></li>
-    </ul>
-  </main>
+      <article>
+        <h2>Meine Sätze im Beet</h2>
+        <ul>
+          <li>Satz #1 in Beet 2 <span>(seit mitte März)</span></li>
+          <li>Satz #2 in Beet 2 <span>(seit Ende März)</span></li>
+          <li>Satz #3 in Beet 2 <span>(seit mitte April)</span></li>
+        </ul>
+      </article>
+    </main>
+  </div>
 </template>
+
 <style scoped>
 main {
   height: 100vh;
@@ -158,6 +161,7 @@ h3 {
 }
 
 .header {
+  margin-top: 2rem;
   display: flex;
   justify-content: start;
   align-items: center;
@@ -165,7 +169,6 @@ h3 {
   padding-block: 0.5rem;
   padding-right: 0.5rem;
 }
-
 .image-container {
   max-width: 200px;
 }
@@ -176,6 +179,7 @@ h3 {
 
 .headline-label {
   text-transform: uppercase;
+  letter-spacing: 0.25rem;
   color: var(--clr-secondary);
   margin-bottom: 0;
 }
@@ -186,24 +190,21 @@ h3 {
 }
 
 .botanical-name {
-  font-size: x-large;
-}
-
-.separator {
-  /* als eigene Komponente definieren? */
-  height: 1px;
-  background-color: var(--clr-secondary);
+  font-size: 1.5rem;
 }
 
 .breadcrum {
   padding-block: 1rem;
 }
-
 .plantcare-details {
   list-style: none;
   display: flex;
   gap: 0.5rem;
   flex-wrap: wrap;
+}
+
+article {
+  margin-block: auto;
 }
 
 .info-wrapper {
