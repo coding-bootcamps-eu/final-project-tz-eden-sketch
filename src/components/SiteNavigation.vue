98e69baf6ce888a029e16f65b5e5cd5e827de1c3
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-10-01 15:05:43 +0200

    Feat: Added site navigation component

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <q-tabs
+      v-model="tab"
+      no-caps
+      outside-arrows
+      mobile-arrows
+      class="site-navigation bg-info text-primary shadow-2"
+      indicator-color="primary"
+    >
+      <q-route-tab name="home" label="Übersicht" icon="bi-house" to="/" />
+      <q-route-tab name="plants" label="Pflanzen" icon="fas fa-leaf" to="/plantspecies/view" />
+      <q-route-tab name="plan" label="Plan" icon="bi-pencil-square" to="/" />
+      <q-route-tab name="knowledge" label="Wissen" icon="bi-book" to="/" />
+    </q-tabs>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const tab = ref('home')
+</script>
+
+<style scoped>
+.site-navigation {
+  border-top: 1px solid var(--clr-secondary);
+}
+</style>
