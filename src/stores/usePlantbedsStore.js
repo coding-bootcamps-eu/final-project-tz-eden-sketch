6ea76de8568960090b068341dc836358a25e5774
wiebken-n <126778886+wiebken-n@users.noreply.github.com>
2023-10-31 12:05:14 +0100

    Changed Filname of store

--- src/stores/usePlantbedsStore.js
+++ src/stores/usePlantbedsStore.js
@@ -2,6 +2,7 @@ import { reactive, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { nanoid } from 'nanoid'
 
+// changed filename
 export const usePlantBedsStore = defineStore('beds', () => {
   const state = reactive({
     currentMonth: 'januar',
