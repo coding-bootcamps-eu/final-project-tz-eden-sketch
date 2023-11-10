38f4412936f3218d6ac9eacf955a3577565213e3
Colourpieces <57894511+Colourpieces@users.noreply.github.com>
2023-11-10 16:45:22 +0100

    refactor: image will load now + update sources

--- src/views/ImpressumView.vue
+++ src/views/ImpressumView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import SeparatorElement from '@/components/SeparatorElement.vue'
 import SiteNavigation from '@/components/SiteNavigation.vue'
 </script>
 
@@ -7,8 +6,15 @@ import SiteNavigation from '@/components/SiteNavigation.vue'
   <main>
     <h1 class="headline-main headline">Impressum</h1>
     <section class="contributers-section">
-      <h2 class="headline-sub">Projektbeteiligte</h2>
-      <p>Diese Webapplikation wurde erstellt durch:</p>
+      <h2 class="headline-sub">Projekt</h2>
+
+      <p>
+        Diese
+        <a href="https://github.com/coding-bootcamps-eu/final-project-tz-eden-sketch">
+          Webapplikation
+        </a>
+        wurde erstellt durch:
+      </p>
       <ul class="contributors-list">
         <li class="contributor">
           <q-icon name="bi-github"></q-icon>
@@ -32,6 +38,7 @@ import SiteNavigation from '@/components/SiteNavigation.vue'
         <li>
           <a href="https://www.freepik.com/">https://www.freepik.com/</a>
         </li>
+        <li>weitere Grafiken erstellt durch Nicole</li>
       </ul>
     </section>
   </main>
@@ -48,16 +55,47 @@ main {
   padding-inline: 2rem;
 
   height: 100vh; /* gibt es eine bessere Lösung? */
-  background-image: url('../src/assets/images/flowers-with-gardening-tools_1000px.svg');
-  background-position: left bottom;
+  background-image: url('/flowers-with-gardening-tools_1000px.svg');
+  background-position: 10% 85%;
   background-size: 35%;
   background-repeat: no-repeat;
 }
+
+@media screen and (max-width: 380px) {
+  /* iPhone SE */
+  h1 {
+    margin: 0;
+  }
+  h2 {
+    margin-bottom: 0.5em;
+  }
+  main {
+    background-size: 35%;
+  }
+}
+
+@media screen and (max-width: 992px) {
+  /* Tablet */
+  main {
+    background-size: 55%;
+  }
+}
+@media screen and (min-width: 993px) {
+  /* Desktop */
+  main {
+    background-size: 35%;
+  }
+}
+
 h1 {
   color: var(--clr-primary);
 }
 h2 {
-  margin-block: 2rem;
+  font-size: 2em;
+}
+
+section + section {
+  margin-top: 2em;
 }
 
 .contributors-list {
